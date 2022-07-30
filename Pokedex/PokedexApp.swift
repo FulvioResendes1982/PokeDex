f3bd5614f2d3ca8e0e7932af91d316bd5fcdb1f4
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-07-31 01:24:27 +0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by ドロケ on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
