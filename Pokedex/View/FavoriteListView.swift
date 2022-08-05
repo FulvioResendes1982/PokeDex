7afa03075530acb55311620b451b59862aa1fd77
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-08-06 02:16:15 +0700

    fix: fix delete bug

--- Pokedex/View/FavoriteListView.swift
+++ Pokedex/View/FavoriteListView.swift
@@ -10,7 +10,7 @@ import Kingfisher
 
 struct FavoriteListView: View {
 //    @ObservedObject var favoritePokemons = FavoritePokemon()
-    var favoritePokemons: FavoritePokemon
+    @ObservedObject var favoritePokemons: FavoritePokemon
     var viewModel: PokemonViewModel
     
     
